125b03554be30eddf32f521a34f1b49c04d7dae1
CBhangs <94818557+CBhangs@users.noreply.github.com>
2022-01-22 10:31:45 -0500

    updated

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hello world")
+const root = document.getElementById("root")
+console.log(root)
+let h1 = document.createElement("h1")
+h1.innerHTML="TicTacToe"
+root.append(h1)
+console.log(h1)
+let h2 = document.createElement("h2")
