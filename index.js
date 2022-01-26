96eaf1528e1002ce5e67aa042f66e2f48b11831a
CBhangs <94818557+CBhangs@users.noreply.github.com>
2022-01-25 23:01:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,84 @@
-let root = document.getElementById("root")
+// get root element 
+function getRoot(){
+  let root = document.getElementById("root")
+  return root;
+}   
 
-let h1 = document.createElement("h1")
-h1.innerHTML="Tic Tac Toe"
-root.append(h1)
-h1.style.textAlign = "center"
-h1.style.fontSize = "70px"
+// clear root content
+function clearRoot(){
+  let root = getRoot();
+  root.replaceChildren();
+}
 
-let h2 =document.createElement("h2")
-h2.innerHTML="Rules of Tic Tac Toe"
-root.append(h2)
-h2.style.textAlign = "left"
-h2.style.fontSize = "30px"
-h2.style.color = "red"
-h2.style.textDecoration = "underline"
+// create title for landing page
+function landingPageTitle(){
+  let root = getRoot();
+  let h1 = document.createElement("h1")
+  h1.innerHTML = "Tic Tac Toe"
+  h1.style.textAlign = "center"
+  h1.style.fontSize = "70px"
+  root.append(h1)
+}
 
+// create title for rules
+function rulesTitle(){
+  let root = getRoot();
+  let h2 = document.createElement("h2")
+  h2.innerHTML = "Rules of Tic Tac Toe"
+  h2.style.textAlign = "left"
+  h2.style.fontSize = "30px"
+  h2.style.color = "red"
+  h2.style.textDecoration = "underline"
+  root.append(h2)
+}
 
-let rules = document.getElementById("rules")
-let ruleList = [ "The game is played on a grid that's 3 squares by 3 squares.", 
-"You are X, your friend is O. Players take turns putting their marks in empty squares.", 
-"The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.",
-"When all 9 squares are full, the game is over.",
-"First to 5 wins"]
+// create ul
+function rulesList(){
+  let root = getRoot();
+  let rules = document.createElement("ul");
+  // add rules to ul
+  let ruleList = ["The game is played on a grid that's 3 squares by 3 squares.",
+    "You are X, your friend is O. Players take turns putting their marks in empty squares.",
+    "The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.",
+    "When all 9 squares are full, the game is over.",
+    "First to 5 wins"]
 
-for (let rule of ruleList) {
+    rules.style.fontSize = "20px"
+
+  for (let rule of ruleList) {
     let newListItem = document.createElement("li")
     newListItem.textContent = rule
-    
     rules.appendChild(newListItem)
+  }
+  root.appendChild(rules)
 }
-rules.style.fontSize = "30px"
 
+// create button
+function landingPageBtn(){
+  let root = getRoot();
+  let btn = document.createElement("button")
+  btn.innerText = "Start Game"
+  btn.style.margin = "auto"
+  btn.style.backgroundColor = "red" 
+  btn.style.color = "white"
+  btn.style.borderRadius = "10px"
+
+  // add eventlistener
+  btn.addEventListener("click", buildPlayerInfoPage)
+
+  root.appendChild(btn)
+}  
+// build landing page
+function buildLandingPage(){
+  clearRoot();
+  landingPageTitle();
+  rulesTitle();
+  rulesList();
+  landingPageBtn();
+}
+
+function buildPlayerInfoPage(){
+  clearRoot();
+}
+
+buildLandingPage()
